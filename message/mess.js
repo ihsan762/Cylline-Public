cfbdd1e47cff582204f211999ffe58d827f77fd0
Fcx-Team <79000992+FcX-Team@users.noreply.github.com>
2022-01-15 13:36:05 +0700

    Add files via upload

---
+++ message/mess.js
@@ -0,0 +1,17 @@
+module.exports.mess = {
+	        wait: '𝑻𝑨𝑲 𝑳𝑶𝑨𝑫𝑰𝑵𝑮 𝑺𝑬𝑲 𝒀𝑨𝑯．．．',
+			success: '𝑺𝑼𝑪𝑪𝑬𝑺𝑺．．．',
+			wrongFormat: 'Format salah, coba liat lagi di menu',
+			error: {
+				api: 'Ups, terjadi kesalahan',
+				stick: 'bukan sticker itu:v',
+				Iv: 'Link tidak valid'
+			},
+			only: {
+				group: 'Maaf! Command ini khusus untuk di dalam Group saja.',
+				admin: 'Maaf! Command ini khusus untuk admin group saja.',
+				premium: 'Kamu bukan user premium, kirim perintah *!buypremium* untuk membeli premium',
+				owner: 'Maaf! Command ini khusus untuk Owner Bot saja.',
+				Badmin: 'Maaf! Command ini khusus untuk Bot ketika jadi admin!!',
+			}
+		}
